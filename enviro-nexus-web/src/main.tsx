584f8c29e1b6575fd0a10f11b3837806e7bf6fec
zoufangfang <zoufangfang@longshine.com>
2026-06-02 14:43:02 +0800

    feat: 前端项目初始化

---
+++ enviro-nexus-web/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource-variable/geist";
+import "./index.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <TooltipProvider>
+      <App />
+      <Toaster />
+    </TooltipProvider>
+  </StrictMode>
+);
